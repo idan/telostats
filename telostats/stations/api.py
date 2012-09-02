cda63e5a0c65f970c037de6e8c8b0578dcc07ef7
Yuval Adam <yuv.adm@gmail.com>
2012-09-02 16:22:03 +0300

    we have station data in api call

--- telostats/stations/api.py
+++ telostats/stations/api.py
@@ -14,9 +14,13 @@ class StationResource(ModelResource):
             'id': ('exact', ),
         }
 
-    def dehydrate(self, bundle):
-        station_id = str(bundle.data['id'])
-        counts = get_latest_counts(station_id)
-        bundle.data['available'] = counts[station_id]['available']
-        bundle.data['poles'] = counts[station_id]['poles']
-        return bundle
+    def alter_list_data_to_serialize(self, request, data):
+        # we're using this method as a hook before the data is serialized back
+        # to the client, that is only called once per call (as opposed to hydrate()
+        # which is called per each object), to inject the availability data
+        counts = get_latest_counts()
+        for bundle in data['objects']:
+            station_id = str(bundle.data['id'])
+            bundle.data['available'] = counts[station_id]['available']
+            bundle.data['poles'] = counts[station_id]['poles']
+        return data
