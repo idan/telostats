1e440c22feb41549703f5021ec0212bbe684314f
Yuval Adam <yuv.adm@gmail.com>
2012-09-02 12:28:51 +0300

    return all stations by default

--- telostats/stations/api.py
+++ telostats/stations/api.py
@@ -7,7 +7,8 @@ class StationResource(ModelResource):
     class Meta:
         queryset = Station.objects.all()
         resource_name = 'station'
+        serializer = Serializer(formats=['json'])
+        limit = 200  # show all stations by default
         filtering = {
             'id': ('exact', ),
         }
-        serializer = Serializer(formats=['json'])
