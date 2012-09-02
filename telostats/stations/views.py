d4596defb6f2b85f34037be19f5a8623f4a6a3bf
Idan Gazit <idan@gazit.me>
2012-09-02 12:23:19 +0300

    Add map page, station list

--- telostats/stations/views.py
+++ telostats/stations/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Station
+
+
+class StationMap(TemplateView):
+    template_name = "map.html"
+
+
+class StationList(ListView):
+    context_object_name = 'stations'
+    model = Station
+
+
+class StationDetail(DetailView):
+    model = Station
+    context_object_name = 'station'
