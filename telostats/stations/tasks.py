1c1e0f2000e814193194e246566cc552336dac3c
Yuval Adam <yuv.adm@gmail.com>
2012-09-09 12:18:23 +0300

    Fixed station update task

--- telostats/stations/tasks.py
+++ telostats/stations/tasks.py
@@ -69,10 +69,13 @@ def parse_stations(raw_stations):
 def store_stations(stations):
     logging.debug("Create/update station metadata in Django DB...")
     for station in stations:
-        fields = ['longitude', 'latitude', 'name', 'address', 'poles', 'available']
-        metadata = dict((f, station[f]) for f in fields)  # not sure if we even need this
-        station, created = Station.objects.get_or_create(
+        fields = ['longitude', 'latitude', 'name', 'address']
+        metadata = dict((f, station[f]) for f in fields)
+        obj, created = Station.objects.get_or_create(
             id=station['id'], defaults=metadata)
+        obj.poles = station['poles']
+        obj.available = station['available']
+        obj.save()
 
 
 def station_poles_key(station):
