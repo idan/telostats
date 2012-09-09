28545545b218ad1b0373c6dfc6b26d94d7e408a7
Yuval Adam <yuv.adm@gmail.com>
2012-09-09 12:02:38 +0300

    Remove alter_list() API hook
    Since we now have the latest poles/available data in DB

--- telostats/stations/api.py
+++ telostats/stations/api.py
@@ -1,7 +1,6 @@
 from tastypie.resources import ModelResource
 from tastypie.serializers import Serializer
 from .models import Station
-from ..utils.tempodb import get_latest_counts
 
 
 class StationResource(ModelResource):
@@ -13,14 +12,3 @@ class StationResource(ModelResource):
         filtering = {
             'id': ('exact', ),
         }
-
-    def alter_list_data_to_serialize(self, request, data):
-        # we're using this method as a hook before the data is serialized back
-        # to the client, that is only called once per call (as opposed to hydrate()
-        # which is called per each object), to inject the availability data
-        counts = get_latest_counts()
-        for bundle in data['objects']:
-            station_id = str(bundle.data['id'])
-            bundle.data['available'] = counts[station_id]['available']
-            bundle.data['poles'] = counts[station_id]['poles']
-        return data
