2bf6f29195c2e2c6079c2f851b337341b1a74629
Idan Gazit <idan@gazit.me>
2012-09-10 14:34:48 +0300

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import local
+
+
+def deploy_staticfiles():
+    local('STATIC_S3=1 ./manage.py collectstatic --noinput')
+
+
+def deploy_heroku():
+    local('git push heroku')
+
+
+def deploy():
+    deploy_staticfiles()
+    deploy_heroku()
