37a2038cb640a546b7707ffd65747182b5c8d90f
Yuval Adam <yuv.adm@gmail.com>
2012-11-19 17:01:33 +0200

    Add redis cache backend

--- telostats/settings/heroku.py
+++ telostats/settings/heroku.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import dj_database_url
+import urlparse
 
 from os import environ
 from .base import *
@@ -9,6 +10,18 @@ ENV = 'HEROKU'
 
 # Store files on S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL'))
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
+        'OPTIONS': {
+            'PASSWORD': redis_url.password,
+            'DB': 1,
+        },
+    },
+}
 
 # Grab database info
 DATABASES = {
